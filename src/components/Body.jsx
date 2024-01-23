b20ecdaa4edb4d230cbabb8325ec141216f83958
Samarth Verma <samarth.verma@stationcasinos.com>
2024-01-23 22:21:08 +0530

    feat: filter button added on body'

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,33 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurantList } from "./../utils/mockData";
+import Button from "./Button";
+import { useState } from "react";
 
 const Body = () => {
+  const [resList, setResList] = useState(restaurantList);
+  const onFilterTopRestaurantButtonClick = () => {
+    setResList((prevList) => {
+      if (prevList.length == restaurantList.length) {
+        return prevList.filter((item) => {
+          return item.info.rating.aggregate_rating >= 4;
+        });
+      } else {
+        return restaurantList;
+      }
+    });
+  };
+
   return (
     <div className="body">
       <div className="search">Search</div>
+      <div className="top-rated-btn-container">
+        <Button
+          title={"Filter Top Restaurant"}
+          onClick={onFilterTopRestaurantButtonClick}
+        />
+      </div>
       <div className="res-container">
-        {restaurantList.map((item) => (
+        {resList.map((item) => (
           <RestaurantCard data={item} key={item.info.resId} />
         ))}
       </div>
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({ title, onClick }) => {
+  return (
+    <button className="btn" onClick={onClick}>
+      {title}
+    </button>
+  );
+};
+
+export default Button;
