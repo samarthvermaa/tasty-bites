0f166ff7c8057e5b1e538fe7807cb3592c961805
Samarth Verma <samarth.verma@stationcasinos.com>
2024-02-03 19:07:10 +0530

    feat: app optmization by hooks and lazy loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import About from "./components/About";
@@ -8,6 +8,7 @@ import Error from "./components/Error";
 import Header from "./components/Header";
 import RestaurantMenu from "./components/RestaurantMenu";
 
+const Grocery = lazy(() => import("./components/Grocery"));
 const AppLayout = () => {
   return (
     <div className="app">
@@ -33,6 +34,14 @@ const appRouter = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading the Grocery App...</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
       },
       {
         path: "/restaurants/:resID",
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,53 @@
+import User from "./User";
+
 const About = () => {
-  return <h1>About us</h1>;
+  return (
+    <div>
+      <h1>About us</h1>;
+      <div className="res-container">
+        <User
+          name="Samarth"
+          image="https://media.licdn.com/dms/image/C4D22AQERP1A7vy1R7g/feedshare-shrink_800/0/1637899925902?e=2147483647&v=beta&t=p9p2mGrSaSCpWvEDttMa0-ePJvUX8_s6YSgQfb2g1HA"
+          location="Noida"
+          age="30"
+          position="Director"
+          rating="5 / 5"
+        />
+        <User
+          name="Shreya"
+          image="https://avatars.githubusercontent.com/u/20899802?v=4"
+          location="Noida"
+          age="26"
+          position="CEO"
+          rating="5 / 5"
+        />
+        <User
+          name="Arvind"
+          image="https://cricheroes.in/assets/images/default-player.jpg"
+          location="Noida"
+          age="57"
+          position="CFO"
+          rating="5 / 5"
+        />
+        <User
+          name="Sangeeta"
+          image="https://i.pinimg.com/736x/ae/45/d1/ae45d19f3216598a4a3cb1afb452a7cd.jpg"
+          location="Noida"
+          age="56"
+          position="CTO"
+          rating="5 / 5"
+        />
+        <User
+          name="Himashi"
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5XgZozAHnaQB3xY7KOIgecje6Bch983XrhA&usqp=CAU"
+          location="Noida"
+          age="56"
+          position="CHR"
+          rating="5 / 5"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default About;
